d258eb9bf4b3d912ee02ef17bca66ca77e9401a2
unknown <pasekamonyeki@gmail.com>
2021-11-01 17:55:29 +0200

    CSV file update and code refactoring

--- src/main/java/com/mycompany/webtonicassessment/DBConnect.java
+++ src/main/java/com/mycompany/webtonicassessment/DBConnect.java
@@ -14,18 +14,21 @@ public class DBConnect {
      public static void main(String args[]){ 
          
          
-    try{  
-    Class.forName("com.mysql.jdbc.Driver");  
-    Connection con=DriverManager.getConnection(  
-    "jdbc:mysql://localhost:3306/csvdb","root","admin?autoReconnect=true&useSSL=false");  
-    //here sonoo is database name, root is username and password  
-    Statement stmt=con.createStatement();  
-    ResultSet rs=stmt.executeQuery("select * from csvdata");  
+    try{
+        
+        Class.forName("com.mysql.jdbc.Driver");  
+        Connection con=DriverManager.getConnection(  
+        "jdbc:mysql://localhost:3306/csvdb","root","admin?autoReconnect=true&useSSL=false");  
+
+        Statement stmt=con.createStatement();  
+        ResultSet rs=stmt.executeQuery("select * from csvdata");  
     while(rs.next())  
-    System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
-    con.close();  
-    }catch(Exception e){ System.out.println(e);} 
-    System.out.println("Hello world");
+        System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+        con.close();  
+    }catch(Exception e){
+        
+        System.out.println(e); 
+    } 
     }
    
 }
